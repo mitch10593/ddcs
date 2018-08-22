2d0bbe3262b19d946e623c758a8812698ae740e0
Mitch <mitch@kilik.fr>
2018-08-22 23:47:04 +0200

    WIP on TANKER command + action

--- src/l10n/DEFAULT/marker.lua
+++ src/l10n/DEFAULT/marker.lua
@@ -11,7 +11,7 @@ marker = {}
 marker.debug = false
 
 ------------------------------------------------------------------------------
--- markerMoveCommandEventHandler
+-- marker.moveCommandEventHandler
 -- ingame marker command: MOVE(groupName,speed)
 -- ex ingame: MOVE(CVN,10)
 ------------------------------------------------------------------------------
@@ -62,6 +62,189 @@ function marker.moveCommandEventHandler(event)
 end
 
 ------------------------------------------------------------------------------
+-- marker.tankerAction
+-- @param string groupName 
+-- @param float fromPositionX
+-- @param float fromPositionY
+-- @param float speed in knots
+-- @param float hdg heading (0-359)
+-- @param float distance in Nm
+-- @param float alt in feet
+------------------------------------------------------------------------------
+function marker.tankerAction(groupName,fromPositionX,fromPositionY,speed,hdg,distance,alt)
+
+	-- prepare LatLong message
+	local fromVec3={x=fromPositionX, y=0, z=fromPositionY}
+	lat, lon = coord.LOtoLL(fromVec3)
+	fromllString = mist.tostringLL(lat, lon, 2)
+	
+	local fromPosition = {
+		["x"] = fromPositionX,
+		["y"] = fromPositionY,
+	}
+
+	local toPosition = {
+		["x"] = fromPositionX + distance * 0.539957 * math.cos(mist.utils.toRadian(hdg-90)),
+		["y"] = fromPositionY + distance * 0.539957 * math.sin(mist.utils.toRadian(hdg-90)),
+	}
+
+	local mission = { 
+		id = 'Mission', 
+		params = { 
+			route = { 
+				points = { 
+					[1] = { 
+						["type"] = "Turning Point",
+						["action"] = "Turning Point",
+						["x"] = fromPosition.x,
+						["y"] = fromPosition.y,
+						["alt"] = alt * 0.3048, -- in meters
+						["alt_type"] = "BARO", 
+						["speed"] = speed/1.94384,  -- speed in m/s
+						["speed_locked"] = boolean, 
+						["task"] = 
+						{
+							["id"] = "ComboTask",
+							["params"] = 
+							{
+								["tasks"] = 
+								{
+									[1] = 
+									{
+										["enabled"] = true,
+										["auto"] = true,
+										["id"] = "Tanker",
+										["number"] = 1,
+									}, -- end of [1]
+									[2] = 
+									{
+										["enabled"] = true,
+										["auto"] = true,
+										["id"] = "WrappedAction",
+										["number"] = 2,
+										["params"] = 
+										{
+											["action"] = 
+											{
+												["id"] = "ActivateBeacon",
+												["params"] = 
+												{
+													["type"] = 4,
+													["AA"] = true,
+													["callsign"] = "TKR",
+													["modeChannel"] = "Y",
+													["channel"] = 1, -- TACAN channel
+													["system"] = 4, -- System = TACAN
+													["bearing"] = true,
+													["frequency"] = 1088000000,
+												}, -- end of ["params"]
+											}, -- end of ["action"]
+										}, -- end of ["params"]
+									}, -- end of [2]
+								}, -- end of ["tasks"]
+							}, -- end of ["params"]
+						}, -- end of ["task"]
+					}, -- enf of [1]
+					[2] = 
+					{
+						["type"] = "Turning Point",
+						["alt"] = alt * 0.3048, -- in meters
+						["action"] = "Turning Point",
+						["alt_type"] = "BARO",
+						["speed"] = speed/1.94384,
+						["speed_locked"] = true,
+						["x"] = toPosition.x,
+						["y"] = toPosition.y,
+						["task"] = 
+						{
+							["id"] = "ComboTask",
+							["params"] = 
+							{
+								["tasks"] = 
+								{
+									[1] = 
+									{
+										["enabled"] = true,
+										["auto"] = false,
+										["id"] = "WrappedAction",
+										["number"] = 1,
+										["params"] = 
+										{
+											["action"] = 
+											{
+												["id"] = "SwitchWaypoint",
+												["params"] = 
+												{
+													["goToWaypointIndex"] = 1,
+													["fromWaypointIndex"] = 2,
+												}, -- end of ["params"]
+											}, -- end of ["action"]
+										}, -- end of ["params"]
+									}, -- end of [1]
+								}, -- end of ["tasks"]
+							}, -- end of ["params"]
+						}, -- end of ["task"]
+					}, -- end of [2]
+				}, 
+			} 
+		} 
+	}
+	
+	local group = Group.getByName(groupName)
+	if group ~= nil then
+		group:getController():setTask(mission)
+		trigger.action.outText(groupName .. ' starting tanker mission ' .. fromllString .. ' hdg ' .. hdg .. ', ' .. alt .. ' ft, speed ' .. (speed/1.94384) .. ' knots' , 10)
+	else
+		trigger.action.outText(groupName .. ' not found' , 10)
+	end
+
+end
+
+------------------------------------------------------------------------------
+-- marker.tankerCommandEventHandler
+-- ingame marker command: TANKER(groupName,speed,hdg,distance,alt)
+-- ex ingame: TANKER(ARCO)
+-- ex ingame: TANKER(ARCO,320)
+------------------------------------------------------------------------------
+function marker.tankerCommandEventHandler(event)
+
+	-- parse the command: TANKER(groupName,speed,hdg,distance,alt)
+	-- params[1]: TANKER
+	-- params[2]: groupName
+	-- params[3]: speed
+	-- params[3]: hdg
+	-- params[3]: distance
+	-- params[3]: alt
+	params = {
+		[1]=nil,
+		[2]=nil,
+		[3]=320,   -- in knots
+		[4]=270,   -- in degrees 0-359
+		[5]=20,    -- in Nm
+		[6]=20000, -- in feet
+	}
+	for v in string.gmatch(event.text, '([^,\(\)]+)') do
+		params[#params+1] = v
+	end
+	
+	local command=params[1]
+	local groupName=params[2]
+	local speed=params[3]
+	local hdg=params[4]
+	local distance=params[5]
+	local alt=params[6]
+	
+	-- not really a TANKER command ?
+	if command ~= "TANKER" then
+		-- just exit, skipping this event
+		return
+	end
+
+	marker.tankerAction(groupName,event.pos.z, event.pos.x, speed, hdg, distance, alt)
+
+end
+
+------------------------------------------------------------------------------
 -- markerDebugEvent
 -- @param Event event : the marker event
 -- display debug informations about this marker event
@@ -100,9 +283,14 @@ function marker.detectMarkers(event)
 		if event.text~=nil and event.text:find('MOVE') then
 			marker.moveCommandEventHandler(event)			
 		end 
-   end 
-end 
 
+		-- handle TANKER command (need to be improved)
+		if event.text~=nil and event.text:find('TANKER') then
+			marker.moveCommandEventHandler(event)			
+		end 
+		
+	end 
+end 
 
 -- in case of testing, remove first previous handlers
 mist.removeEventHandler(markerDetectMarkersEventHandler)
